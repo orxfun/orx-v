477ae0cec4ec21ecec92812f30d1435def891ae2
orxfun <orx.ugur.arikan@gmail.com>
2026-09-17 23:16:05 +0200

    impl cloned

--- src/redesign/copied_cloned.rs
+++ src/redesign/copied_cloned.rs
@@ -1,6 +1,8 @@
 use super::{Dim, NVec};
 use core::marker::PhantomData;
 
+// copied
+
 pub struct Copied<'a, D, T, V>(V, PhantomData<&'a (D, T)>)
 where
     D: Dim,
@@ -15,5 +17,24 @@ where
 {
     fn at(&self, idx: <D as Dim>::Idx) -> T {
         *self.0.at(idx)
+    }
+}
+
+// cloned
+
+pub struct Cloned<'a, D, T, V>(V, PhantomData<&'a (D, T)>)
+where
+    D: Dim,
+    T: Clone + 'a,
+    V: NVec<D, &'a T>;
+
+impl<'a, D, T, V> NVec<D, T> for Cloned<'a, D, T, V>
+where
+    D: Dim,
+    T: Clone + 'a,
+    V: NVec<D, &'a T>,
+{
+    fn at(&self, idx: <D as Dim>::Idx) -> T {
+        self.0.at(idx).clone()
     }
 }
