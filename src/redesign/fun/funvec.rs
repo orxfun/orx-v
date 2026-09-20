13f1ef28f846e82785b190e47391c50bc9327f95
orxfun <orx.ugur.arikan@gmail.com>
2026-09-20 21:51:17 +0200

    impl funvec

--- src/redesign/fun/funvec.rs
+++ src/redesign/fun/funvec.rs
@@ -1,1 +1,22 @@
+use super::super::{Dim, NVec};
+use core::marker::PhantomData;
 
+pub struct FunVec<D, T, F>
+where
+    D: Dim,
+    F: Fn(D::Idx) -> T,
+{
+    fun: F,
+    p: PhantomData<(D, T)>,
+}
+
+impl<D, T, F> NVec<D, T> for FunVec<D, T, F>
+where
+    D: Dim,
+    F: Fn(D::Idx) -> T,
+{
+    #[inline(always)]
+    fn at(&self, idx: <D as Dim>::Idx) -> T {
+        (self.fun)(idx)
+    }
+}
