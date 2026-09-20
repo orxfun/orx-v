49809376eb3e010e6775a20ee09e01960f831778
orxfun <orx.ugur.arikan@gmail.com>
2026-09-20 22:56:17 +0200

    wip

--- src/redesign/impl_nvec2/tests/vec.rs
+++ src/redesign/impl_nvec2/tests/vec.rs
@@ -7,18 +7,10 @@ use alloc::vec;
 fn vec_as_v2() {
     let mut v = vec![vec![0], vec![1, 2, 3], vec![4, 5]];
 
-    let x = second_first::<&i32>(&v);
-    // assert_eq!(second_first(&v), &1);
+    let x = second_first(&v);
 
     assert_eq!(second_first_ref(&v), &1);
     assert_eq!(second_first_mut(&mut v), &mut 1);
-
-    // assert_eq!(second(&&v), &2);
-    // assert_eq!(second(&v.cloned()), 2);
-    // assert_eq!(second(&v.copied()), 2);
-
-    // assert_eq!(second_ref(&v), &2);
-    // assert_eq!(second_mut(&mut v), &mut 2);
 }
 
 // #[test]
