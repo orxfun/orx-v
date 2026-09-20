3798871db4d2d69c8e348bf16338d9b81a3b17c8
orxfun <orx.ugur.arikan@gmail.com>
2026-09-20 19:16:21 +0200

    add scalar tests

--- src/redesign/impl_nvec0.rs
+++ src/redesign/impl_nvec0.rs
@@ -9,9 +9,28 @@ impl<'a, T> NVec<D0, &'a T> for &'a T {
 
 #[cfg(test)]
 mod tests {
+    use alloc::string::ToString;
+
     use super::*;
 
     fn get<T>(v: impl NVec<D0, T>) -> T {
         v.at([])
+    }
+
+    #[test]
+    fn scalar_as_v0_copy() {
+        let a = 12;
+
+        assert_eq!(get(&a), &12);
+        assert_eq!(get(a.cloned()), 12);
+        assert_eq!(get(a.copied()), 12);
+    }
+
+    #[test]
+    fn scalar_as_v0_clone() {
+        let a = "x".to_string();
+
+        assert_eq!(get(&a), &"x".to_string());
+        assert_eq!(get(a.cloned()), "x".to_string());
     }
 }
