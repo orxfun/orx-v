3252cbafe92fc81e72b2806952fe34ea77741a6a
orxfun <orx.ugur.arikan@gmail.com>
2026-09-20 22:55:46 +0200

    wip nvec-2

--- src/redesign/impl_nvec2/tests/vec.rs
+++ src/redesign/impl_nvec2/tests/vec.rs
@@ -1,5 +1,5 @@
 use super::super::super::NVec;
-use super::utils::{second_first, second_first_mut, second_first_ref};
+use super::utils::{second_first, second_first_mut, second_first_ref, second_first2};
 use alloc::string::ToString;
 use alloc::vec;
 
@@ -7,6 +7,7 @@ use alloc::vec;
 fn vec_as_v2() {
     let mut v = vec![vec![0], vec![1, 2, 3], vec![4, 5]];
 
+    let x = second_first::<&i32>(&v);
     // assert_eq!(second_first(&v), &1);
 
     assert_eq!(second_first_ref(&v), &1);
--- src/redesign/impl_nvec2/vec.rs
+++ src/redesign/impl_nvec2/vec.rs
@@ -1,4 +1,4 @@
-use super::super::{D1, D2, Dim, NVecMut, NVecRef};
+use super::super::{D1, D2, Dim, NVec, NVecMut, NVecRef};
 use alloc::vec::Vec;
 
 impl<T, V: NVecRef<D1, T>> NVecRef<D2, T> for Vec<V> {
@@ -10,5 +10,15 @@ impl<T, V: NVecRef<D1, T>> NVecRef<D2, T> for Vec<V> {
 impl<T, V: NVecMut<D1, T>> NVecMut<D2, T> for Vec<V> {
     fn at_mut(&mut self, [i, j]: <D2 as Dim>::Idx) -> &mut T {
         self[i].at_mut(j)
+    }
+}
+
+impl<'a, T, V> NVec<D2, &'a T> for &'a Vec<V>
+where
+    &'a V: NVec<D1, &'a T>,
+{
+    fn at(&self, [i, j]: <D2 as Dim>::Idx) -> &'a T {
+        let child: &'a V = &self[i];
+        child.at(j)
     }
 }
