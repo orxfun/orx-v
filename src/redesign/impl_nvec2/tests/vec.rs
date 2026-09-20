627394e9a7fbcfacf3d408e10e56733d852dd4c8
orxfun <orx.ugur.arikan@gmail.com>
2026-09-20 22:06:51 +0200

    wip

--- src/redesign/impl_nvec2/tests/vec.rs
+++ src/redesign/impl_nvec2/tests/vec.rs
@@ -1,7 +1,24 @@
 use super::super::super::NVec;
-use super::utils::second_first;
+use super::utils::{second_first, second_first_mut, second_first_ref};
 use alloc::string::ToString;
 use alloc::vec;
+
+#[test]
+fn vec_as_v2() {
+    let mut v = vec![vec![0], vec![1, 2, 3], vec![4, 5]];
+
+    assert_eq!(second_first(&v), &1);
+
+    assert_eq!(second_first_ref(&v), &1);
+    assert_eq!(second_first_mut(&mut v), &mut 1);
+
+    // assert_eq!(second(&&v), &2);
+    // assert_eq!(second(&v.cloned()), 2);
+    // assert_eq!(second(&v.copied()), 2);
+
+    // assert_eq!(second_ref(&v), &2);
+    // assert_eq!(second_mut(&mut v), &mut 2);
+}
 
 // #[test]
 // fn vec_vec_as_v1_copy() {
