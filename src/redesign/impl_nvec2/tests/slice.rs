d0ef478602258369ca741d08fd21798725fb21bd
orxfun <orx.ugur.arikan@gmail.com>
2026-09-20 18:17:16 +0200

    impl slice of slice v2 tests

--- src/redesign/impl_nvec2/tests/slice.rs
+++ src/redesign/impl_nvec2/tests/slice.rs
@@ -24,3 +24,16 @@ fn slice_vec_as_v1_clone() {
     assert_eq!(second_first(&slice_vec), &"y".to_string());
     assert_eq!(second_first(slice_vec.cloned()), "y".to_string());
 }
+
+#[test]
+fn slice_slice_as_v1_copy() {
+    let a = vec![0];
+    let b = vec![1, 2, 3];
+    let c = vec![4, 5];
+    let vec_slice = vec![a.as_slice(), &b, &c];
+    let slice_slice = vec_slice.as_slice();
+
+    assert_eq!(second_first(&slice_slice), &1);
+    assert_eq!(second_first(slice_slice.cloned()), 1);
+    assert_eq!(second_first(slice_slice.copied()), 1);
+}
