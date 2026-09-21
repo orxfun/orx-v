a9526a75336423b8f894f66601fcc91fc66a4785
orxfun <orx.ugur.arikan@gmail.com>
2026-09-21 15:52:40 +0200

    rename fun vec modules

--- src/redesign/fun/mod.rs
+++ src/redesign/fun/mod.rs
@@ -1,14 +1,14 @@
 #[cfg(test)]
 mod tests;
 
+mod fun_vec1;
+mod fun_vec2;
+mod fun_vec3;
+mod fun_vec4;
 mod funvec;
-mod funvec1;
-mod funvec2;
-mod funvec3;
-mod funvec4;
 
+pub use fun_vec1::{FunVec1, FunVec1ChildOfD2, FunVec1ChildOfD3, FunVec1ChildOfD4};
+pub use fun_vec2::{FunVec2, FunVec2ChildOfD3, FunVec2ChildOfD4};
+pub use fun_vec3::{FunVec3, FunVec3ChildOfD4};
+pub use fun_vec4::FunVec4;
 pub use funvec::FunVec;
-pub use funvec1::{FunVec1, FunVec1ChildOfD2, FunVec1ChildOfD3, FunVec1ChildOfD4};
-pub use funvec2::{FunVec2, FunVec2ChildOfD3, FunVec2ChildOfD4};
-pub use funvec3::{FunVec3, FunVec3ChildOfD4};
-pub use funvec4::FunVec4;
--- src/redesign/nvec.rs
+++ src/redesign/nvec.rs
@@ -35,3 +35,9 @@ pub trait V<D: Dim, T> {
         unreachable!()
     }
 }
+
+pub trait Vm<D: Dim, T> {
+    fn at(&self, idx: D::Idx) -> &T;
+
+    fn mut_at(&mut self, idx: D::Idx) -> &mut T;
+}
