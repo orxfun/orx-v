8d35355faf0da2e48b93d64701521f7d3f9c175e
orxfun <orx.ugur.arikan@gmail.com>
2026-09-21 15:50:32 +0200

    add copied tests to funvec2

--- src/redesign/fun/tests/v2.rs
+++ src/redesign/fun/tests/v2.rs
@@ -14,16 +14,30 @@ fn reduce_first_child<'a, T: 'a>(v: impl V<D2, &'a T>, mut init: T, f: impl Fn(T
     init
 }
 
+fn reduce_first_child_copied<T>(v: impl V<D2, T>, mut init: T, f: impl Fn(T, T) -> T) -> T {
+    let c = v.child(1);
+    for j in 0..3 {
+        init = f(init, c.at(j));
+    }
+    init
+}
+
 #[test]
 fn vec_vec_as_v2() {
     let data = vec![vec![0], vec![1, 2, 3], vec![4, 5]];
 
     let f = |[i, j]: [usize; 2]| &data[i][j];
-
     let v = FunVec2::new(&f);
 
     assert_eq!(second_first(&v), &1);
     assert_eq!(reduce_first_child(&v, 0, |a, b| a + b), 6);
+
+    // copied
+    let f = |[i, j]: [usize; 2]| data[i][j];
+    let v = FunVec2::new(&f);
+
+    assert_eq!(second_first(&v), 1);
+    assert_eq!(reduce_first_child_copied(&v, 0, |a, b| a + b), 6);
 }
 
 #[test]
@@ -34,11 +48,17 @@ fn vec_slice_as_v2() {
     let data = vec![a.as_slice(), b.as_slice(), c.as_slice()];
 
     let f = |[i, j]: [usize; 2]| &data[i][j];
-
     let v = FunVec2::new(&f);
 
     assert_eq!(second_first(&v), &1);
     assert_eq!(reduce_first_child(&v, 0, |a, b| a + b), 6);
+
+    // copied
+    let f = |[i, j]: [usize; 2]| data[i][j];
+    let v = FunVec2::new(&f);
+
+    assert_eq!(second_first(&v), 1);
+    assert_eq!(reduce_first_child_copied(&v, 0, |a, b| a + b), 6);
 }
 
 #[test]
@@ -49,11 +69,17 @@ fn vec_vecref_as_v2() {
     let data = vec![&a, &b, &c];
 
     let f = |[i, j]: [usize; 2]| &data[i][j];
-
     let v = FunVec2::new(&f);
 
     assert_eq!(second_first(&v), &1);
     assert_eq!(reduce_first_child(&v, 0, |a, b| a + b), 6);
+
+    // copied
+    let f = |[i, j]: [usize; 2]| data[i][j];
+    let v = FunVec2::new(&f);
+
+    assert_eq!(second_first(&v), 1);
+    assert_eq!(reduce_first_child_copied(&v, 0, |a, b| a + b), 6);
 }
 
 #[test]
@@ -65,9 +91,15 @@ fn slice_vecref_as_v2() {
     let data = vec.as_slice();
 
     let f = |[i, j]: [usize; 2]| &data[i][j];
-
     let v = FunVec2::new(&f);
 
     assert_eq!(second_first(&v), &1);
     assert_eq!(reduce_first_child(&v, 0, |a, b| a + b), 6);
+
+    // copied
+    let f = |[i, j]: [usize; 2]| data[i][j];
+    let v = FunVec2::new(&f);
+
+    assert_eq!(second_first(&v), 1);
+    assert_eq!(reduce_first_child_copied(&v, 0, |a, b| a + b), 6);
 }
