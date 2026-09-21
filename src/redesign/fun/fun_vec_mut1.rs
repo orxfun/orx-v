3a6de5e811f255f7eebcba42d4d789ac60b91956
orxfun <orx.ugur.arikan@gmail.com>
2026-09-21 16:14:37 +0200

    generalize FunMutVec1

--- src/redesign/fun/fun_vec_mut1.rs
+++ src/redesign/fun/fun_vec_mut1.rs
@@ -2,30 +2,37 @@ use super::super::{D1, D2, D3, D4, Dim, NVecNever, Vm};
 use core::marker::PhantomData;
 use orx_self_or::SoM;
 
-pub struct FunMutVec1<D, S, T, F>(S, F, PhantomData<(D, T)>)
+pub struct FunMutVec1<D, S, T, Fr, Fm>(S, Fr, Fm, PhantomData<(D, T)>)
 where
     S: SoM<D>,
-    F: Fn(&mut D, <D1 as Dim>::Idx) -> &mut T;
+    Fr: Fn(&D, <D1 as Dim>::Idx) -> &T,
+    Fm: Fn(&mut D, <D1 as Dim>::Idx) -> &mut T;
 
-impl<D, S, T, F> FunMutVec1<D, S, T, F>
+impl<D, S, T, Fr, Fm> FunMutVec1<D, S, T, Fr, Fm>
 where
     S: SoM<D>,
-    F: Fn(&mut D, <D1 as Dim>::Idx) -> &mut T,
+    Fr: Fn(&D, <D1 as Dim>::Idx) -> &T,
+    Fm: Fn(&mut D, <D1 as Dim>::Idx) -> &mut T,
 {
-    pub fn new(data: S, f: F) -> Self {
-        Self(data, f, PhantomData)
+    pub fn new(data: S, g: Fr, m: Fm) -> Self {
+        Self(data, g, m, PhantomData)
     }
 }
 
 // impl Vm
 
-impl<D, S, T, F> Vm<D1, T> for FunMutVec1<D, S, T, F>
+impl<D, S, T, Fr, Fm> Vm<D1, T> for FunMutVec1<D, S, T, Fr, Fm>
 where
     S: SoM<D>,
-    F: Fn(&mut D, <D1 as Dim>::Idx) -> &mut T,
+    Fr: Fn(&D, <D1 as Dim>::Idx) -> &T,
+    Fm: Fn(&mut D, <D1 as Dim>::Idx) -> &mut T,
 {
+    fn at(&self, idx: <D1 as Dim>::Idx) -> &T {
+        (self.1)(self.0.get_ref(), idx)
+    }
+
     fn mut_at(&mut self, idx: <D1 as Dim>::Idx) -> &mut T {
-        (self.1)(self.0.get_mut(), idx)
+        (self.2)(self.0.get_mut(), idx)
     }
 }
 
@@ -39,12 +46,18 @@ mod tests {
     fn abc() {
         let mut data = vec![1, 2, 3];
 
-        fn f<'a>(d: &'a mut Vec<i32>, i: usize) -> &'a mut i32 {
+        fn g<'a>(d: &'a Vec<i32>, i: usize) -> &'a i32 {
+            &d[i]
+        }
+
+        fn m<'a>(d: &'a mut Vec<i32>, i: usize) -> &'a mut i32 {
             &mut d[i]
         }
 
-        let mut v = FunMutVec1::new(&mut data, f);
+        let mut v = FunMutVec1::new(&mut data, g, m);
+        assert_eq!(v.at(1), &2);
         *v.mut_at(1) = 22;
+        assert_eq!(v.at(1), &22);
 
         assert_eq!(data, &[1, 22, 3]);
     }
--- src/redesign/nvec.rs
+++ src/redesign/nvec.rs
@@ -37,5 +37,7 @@ pub trait V<D: Dim, T> {
 }
 
 pub trait Vm<D: Dim, T> {
+    fn at(&self, idx: D::Idx) -> &T;
+
     fn mut_at(&mut self, idx: D::Idx) -> &mut T;
 }
