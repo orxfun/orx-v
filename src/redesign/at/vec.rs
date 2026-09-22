5f52cc9f15b371645fe9ecfa54cde73685a28c90
orxfun <orx.ugur.arikan@gmail.com>
2026-09-22 10:32:33 +0200

    impl at-d2 for vec

--- src/redesign/at/vec.rs
+++ src/redesign/at/vec.rs
@@ -1,8 +1,14 @@
-use super::super::D1;
+use super::super::{D1, D2};
 use super::At;
 use alloc::vec::Vec;
 
 // d1
+
+impl<'a, T> At<D1, &'a T> for &'a Vec<T> {
+    fn at(&self, idx: usize) -> &'a T {
+        &self[idx]
+    }
+}
 
 impl<T: Copy> At<D1, T> for Vec<T> {
     fn at(&self, idx: usize) -> T {
@@ -10,8 +16,22 @@ impl<T: Copy> At<D1, T> for Vec<T> {
     }
 }
 
-impl<'a, T> At<D1, &'a T> for &'a Vec<T> {
-    fn at(&self, idx: usize) -> &'a T {
-        &self[idx]
+// d2
+
+impl<'a, T, C1> At<D2, &'a T> for &'a Vec<C1>
+where
+    C1: At<D1, &'a T>,
+{
+    fn at(&self, [i, j]: [usize; 2]) -> &'a T {
+        self[i].at(j)
+    }
+}
+
+impl<T, C1> At<D2, T> for Vec<C1>
+where
+    C1: At<D1, T>,
+{
+    fn at(&self, [i, j]: [usize; 2]) -> T {
+        self[i].at(j)
     }
 }
